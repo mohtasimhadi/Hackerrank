d4c41cf9a17f3f7e3d69df623aff0663723ef18d
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2020-01-29 02:54:16 +0600

    Create Conditional Statement.cpp

---
+++ Conditional Statement.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    switch(n){
+        case 1:
+            cout<<"one"<<endl;
+            return 0;
+        case 2:
+            cout<<"two"<<endl;
+            return 0;
+        case 3:
+            cout<<"three"<<endl;
+            return 0;
+        case 4:
+            cout<<"four"<<endl;
+            return 0;
+        case 5:
+            cout<<"five"<<endl;
+            return 0;
+        case 6:
+            cout<<"six"<<endl;
+            return 0;
+        case 7:
+            cout<<"seven"<<endl;
+            return 0;
+        case 8:
+            cout<<"eight"<<endl;
+            return 0;
+        case 9:
+            cout<<"nine"<<endl;
+            return 0;
+        default:
+            cout<<"Greater than 9"<<endl;
+            return 0;
+    }
+
+    return 0;
+}
